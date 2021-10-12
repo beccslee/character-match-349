b26ca840a35d7d583b27950d88d9a29dd37d5a86
Stephen Landaas <stephenlandaas@gmail.com>
2021-10-11 18:07:21 -0700

    Added nav and header components, general testing/setup

---
+++ components/Nav.js
@@ -0,0 +1,11 @@
+import styles from "../styles/Nav.module.css";
+
+function Nav() {
+    return (
+        <div className={styles.navbar}>
+            This is a navbar
+        </div>
+    );
+}
+
+export default Nav;
