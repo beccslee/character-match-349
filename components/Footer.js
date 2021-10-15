6c43b93e47d113f1052fea6286a50b14c6b93ead
Ali Hussain <ali25311@hotmail.com>
2021-10-15 11:30:33 -0700

    created/added footer, also included font-awesome

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,24 @@
 import styles from "../styles/Footer.module.css";
+import "../node_modules/font-awesome/css/font-awesome.min.css";
 
 function Footer() {
-    return (
-        <div className={styles.testing}>FOOTER HERE</div>
-    );
+  return (
+    <div className={styles.footer}>
+      <div id={styles.button}>
+        <a href="#top">
+          <i className="fa fa-arrow-circle-up fa-4x"></i>
+        </a>
+      </div>
+      <div className={styles.footerText}>
+        Team Barry | CPSC 349 - Section 01 | Fall Semester, 2021
+        <div className={styles.footerText2}>
+          All characters presented are not property of our team, and they all
+          belong to their respective franchies. Their use is for entertainment &
+          educational purposes only.
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
