410883ba5e6f8e259c707d8ad4f8689f8665b0e9
Ali Hussain <ali25311@hotmail.com>
2021-10-23 11:14:33 -0700

    Added GameCard elements and styling

--- components/GameCard.js
+++ components/GameCard.js
@@ -1,10 +1,13 @@
-import styles from "../styles/GameCard.module";
+import styles from "../styles/GameCard.module.css";
 
 function GameCard() {
   return (
-  <div className={styles.cardContainer}>
-      {/* to be made */}
-  </div>
+    <div className={styles.gamecardContainer}>
+      <h1 className={styles.title}>
+        <span>WORMY</span>
+      </h1>
+      <footer className={styles.bottom}></footer>
+    </div>
   );
 }
 
--- pages/arena.js
+++ pages/arena.js
@@ -1,14 +1,19 @@
 import styles from "../styles/arena.module.css";
 import "../node_modules/font-awesome/css/font-awesome.min.css";
+import GameCard from "../components/GameCard";
 
 export default function Home() {
   return (
     <div className={styles.arenaContainer}>
       <div className={styles.headingContainer}>
-        <button className={styles.backButton}><i class="fa fa-arrow-left"></i></button>
-        <h1 className={styles.arenaHeader}>A<span className={styles.letter1}>R</span>EN<span className={styles.letter2}>A</span></h1>
+        <button className={styles.backButton}>
+          <i class="fa fa-arrow-left"></i>
+        </button>
+        <h1 className={styles.arenaHeader}>
+          A<span className={styles.letter1}>R</span>EN
+          <span className={styles.letter2}>A</span>
+        </h1>
       </div>
-        
     </div>
   );
 }
