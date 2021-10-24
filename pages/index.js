a37a9e0363d3978a531a87d12909a573f969f88f
Rebecca Lee <beccs1990@gmail.com>
2021-10-23 19:46:59 -0700

    NavBar active section styling on scroll events

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import React, { useRef } from 'react';
 import Image from "next/image";
 import Nav from "../components/Nav";
 import Banner from "../components/Banner";
@@ -7,13 +8,41 @@ import Footer from "../components/Footer";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  // Create forward references to each section so NavBar can refer
+  const playRef = useRef();
+  const submitRef = useRef();
+  const aboutRef = useRef();
+
+  const menuRefs = [
+    {
+      sectionID: 'play',
+      sectionRef: playRef,
+      sectionTitle: 'PLAY NOW'
+    },
+    {
+      sectionID: 'submit',
+      sectionRef: submitRef,
+      sectionTitle: 'SUBMIT A CHARACTER'
+    },
+    {
+      sectionID: 'about',
+      sectionRef: aboutRef,
+      sectionTitle: 'ABOUT'
+    },
+  ]
+
   return (
-    
     <div className={styles.contentContainer}>
-      <Nav/>
-      <Banner id="play"/>
-      <SubmitCharacter id="submit"/>
-      <About id="about"/>
+      <Nav menuRefs={menuRefs} />
+      <div className="section" id="play" ref={playRef}>
+        <Banner/>
+      </div>
+      <div className="section" id="submit" ref={submitRef} >
+        <SubmitCharacter/>
+      </div>
+      <div className="section" id="about" ref={aboutRef}>
+        <About/>
+      </div>
       <Footer/>
     </div>
   );
