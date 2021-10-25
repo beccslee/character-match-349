418dc9b592ecd84f0b29d0ac96b4d92c749b873d
Stephen Landaas <stephenlandaas@gmail.com>
2021-10-24 17:47:43 -0700

    Styling adjustments

--- components/GameCard.js
+++ components/GameCard.js
@@ -3,7 +3,7 @@ import styles from "../styles/GameCard.module.css";
 function GameCard({characterName, imgSrc}) {
 	return (
 		<div className={styles.gamecardContainer} style={{backgroundImage: `url(${imgSrc})`}}>
-			<h1 className={styles.title} style={{fontSize: `${characterName.length > 10 ? "calc(0.15rem + 3vw)" : "calc(1rem + 3vw)"}`}}>
+			<h1 className={styles.title}>
 				<span>{characterName}</span>
 			</h1>
 			<footer className={styles.bottom}></footer>
